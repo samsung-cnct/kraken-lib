fd18010788b4bd4933ef0abfe368da9c6ff8107c
Lee Chang <leetchang@gmail.com>
2015-04-07 23:07:56 -0700

    add export helper

---
+++ flannel/export.sh
@@ -0,0 +1,6 @@
+echo "Applying local environment settings"
+# Setup environment variables
+export ETCDCTL_PEERS=http://172.16.1.101:4001
+export PATH=/opt/kubernetes/platforms/darwin/amd64:$PATH
+export FLEETCTL_ENDPOINT=http://172.16.1.101:4001
+export KUBERNETES_MASTER=http://172.16.1.101:8080
