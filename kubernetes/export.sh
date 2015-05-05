175055b22144f65a94d56ab71dd812de4d81c1a3
Lee Chang <leetchang@gmail.com>
2015-05-05 13:36:19 -0700

    remove deprecated files

--- kubernetes/export.sh
+++
@@ -1,7 +0,0 @@
-
-echo "Applying local environment settings"
-# Setup environment variables
-export ETCDCTL_PEERS=http://172.16.1.101:4001
-export PATH=/opt/kubernetes/platforms/darwin/amd64:$PATH
-export FLEETCTL_ENDPOINT=http://172.16.1.102:4001
-export KUBERNETES_MASTER=http://172.16.1.102:8080
