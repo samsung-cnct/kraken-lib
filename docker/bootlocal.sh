ac164e7ff960a32190bf3b98e10c3186bac8cd61
Marat G <maratoid@gmail.com>
2015-09-11 16:59:14 -0700

    get rid of unused files. use simple service without remainonexit for ansible runs (should get restart on error working)

--- docker/bootlocal.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# Disable Docker TLS for development and testing purposes
-sudo tee /var/lib/boot2docker/profile <<EOF
-DOCKER_TLS="no"
-EXTRA_ARGS="--insecure-registry dockerrepo.nl.novamedia.com "
-EOF
-sudo /etc/init.d/docker restart
