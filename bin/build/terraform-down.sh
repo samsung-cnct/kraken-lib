1d122a47b87d816aa44e9f75e3ac257c8c9c87b0
Aaron Crickenberger <spiffxp@gmail.com>
2016-03-30 11:25:25 -0700

    Prepend `time` invocation to terraform
    I could do it on my own but if we've already got terraform dumping
    some output about how many resources changed, why not include how
    long it took to change those resources

--- bin/build/terraform-down.sh
+++ bin/build/terraform-down.sh
@@ -21,7 +21,7 @@ if [ -z ${KRAKEN_CLUSTER_NAME+x} ]; then
 fi
 
 max_retries=10
-until terraform destroy -force -input=false \
+time until terraform destroy -force -input=false \
   -var-file=/opt/kraken/terraform/${KRAKEN_CLUSTER_TYPE}/${KRAKEN_CLUSTER_NAME}/terraform.tfvars \
   -state=/kraken_data/${KRAKEN_CLUSTER_NAME}/terraform.tfstate \
   -var 'cluster_name=${KRAKEN_CLUSTER_NAME}' /opt/kraken/terraform/${KRAKEN_CLUSTER_TYPE}
