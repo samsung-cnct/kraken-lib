95b75d00d28dfa7745ff06a8f1972b4e0b594cea
David Watson <davidewatson@gmail.com>
2016-05-17 10:10:32 -0700

    Support all regions by making the creation of subnets dynamic based on the number of availability zones (KRAK-362).

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -52,6 +52,13 @@ variable "aws_region_azs" {
   default = {
     "us-west-2" = "us-west-2a,us-west-2b,us-west-2c"
     "us-east-1" = "us-east-1b,us-east-1c,us-east-1d"
+    "us-west-1" = "us-west-1a,us-west-1b"
+    "eu-west-1" = "eu-west-1a,eu-west-1b,eu-west-1c"
+    "eu-central-1" = "eu-central-1a,eu-central-1b"
+    "ap-southeast-1" = "ap-southeast-1a,ap-southeast-1b"
+    "ap-southeast-2" = "ap-southeast-2a,ap-southeast-2b,ap-southeast-2c"
+    "ap-northeast-1" = "ap-northeast-1a,ap-northeast-1c"
+    "sa-east-1" = "sa-east-1a,sa-east-1b,sa-east-1c"
   }
 
   description = "Availability zones by region"
