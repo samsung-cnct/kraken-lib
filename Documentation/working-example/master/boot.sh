2869addd017772ef1351d602433414bdfb5194ed
Michael Venezia <michael.venezia@viacom.com>
2016-08-24 19:07:07 -0400

    Fixing various things

---
+++ Documentation/working-example/master/boot.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+aws ec2 run-instances --count 1 --image-id ami-6d138f7a \
+      --subnet-id subnet-d90f3df3 \
+      --security-group-ids sg-09cbdc72 \
+      --region us-east-1 \
+      --key-name venezia-testing \
+      --instance-type m3.medium \
+      --block-device-mapping '{"DeviceName":"/dev/xvdf","Ebs": {"VolumeSize":10}}' \
+      --user-data file://./cloud-config.yaml
