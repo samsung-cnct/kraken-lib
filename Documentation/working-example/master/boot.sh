4fbfa10e4855048ca19add08d897c4093d92b206
Michael Venezia <michael.venezia@viacom.com>
2016-08-25 14:42:15 -0400

    Work on master

--- Documentation/working-example/master/boot.sh
+++ Documentation/working-example/master/boot.sh
@@ -5,5 +5,6 @@ aws ec2 run-instances --count 1 --image-id ami-6d138f7a \
       --region us-east-1 \
       --key-name venezia-testing \
       --instance-type m3.medium \
+      --iam-instance-profile Name="KubernetesMaster" \
       --block-device-mapping '{"DeviceName":"/dev/xvdf","Ebs": {"VolumeSize":10}}' \
       --user-data file://./cloud-config.yaml
