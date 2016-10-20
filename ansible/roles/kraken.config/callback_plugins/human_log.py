ce87388658714eb21aadf5e244904d239059f988
Marat G <maratoid@gmail.com>
2016-10-20 11:19:39 -0700

    fix pythonpath for virtualenv

--- ansible/roles/kraken.config/callback_plugins/human_log.py
+++ ansible/roles/kraken.config/callback_plugins/human_log.py
@@ -33,7 +33,7 @@ class CallbackModule(CallbackBase):
         if type(data) == dict:
             for field in FIELDS:
                 if field in data.keys() and data[field]:
-                    output = self._format_output(data[field])
+                    output = str(self._format_output(data[field]))
                     print("\n{0}: {1}".format(field, output.replace("\\n","\n")))
 
     def _format_output(self, output):
