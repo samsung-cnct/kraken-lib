ab4c1eebe19a840e546a01764eef15d76812f7bd
Patrick Christopher <coffeepac@gmail.com>
2016-11-17 16:39:19 -0800

    etcd dynamic resizing removal
    this commit contains the deletes necessary for the etcd dynmic
    resizing removal
    also contains updates to .gitignore to ignore some more stuff

--- ansible/roles/kraken.provider/kraken.provider.aws/test/lambda/up.sh
+++ ansible/roles/kraken.provider/kraken.provider.aws/test/lambda/up.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-echo "ZIP the lambda source"
-cp ../../files/lambda-sns-etcd-service-discovery.py .
-zip ./lambda-sns-etcd-service-discovery.zip lambda-sns-etcd-service-discovery.py
-rm lambda-sns-etcd-service-discovery.py
-
 echo "Run terraform 0.7.x"
 terraform apply
