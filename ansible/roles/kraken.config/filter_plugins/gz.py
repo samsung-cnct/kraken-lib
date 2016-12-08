1159b070a2ad49a36467550351f333673351c04e
Patrick Christopher <coffeepac@gmail.com>
2016-12-08 14:44:01 -0800

    make gz calls idemptotent
    force all calls to gzip to happen an arbitrary, fixed time

--- ansible/roles/kraken.config/filter_plugins/gz.py
+++ ansible/roles/kraken.config/filter_plugins/gz.py
@@ -1,9 +1,10 @@
 import StringIO
 import gzip
+import time
 
 def gz(content_to_compress):
  	out = StringIO.StringIO()
-	with gzip.GzipFile(fileobj=out, mode="w") as f:
+	with gzip.GzipFile(fileobj=out, mode="w", mtime=1481236323.771718) as f:  # parameter to mtime is chosen at random but should be same for every call
 		f.write(content_to_compress)
 	return out.getvalue()
 
