16fd0c8b71bffd211ce0f0a8083b503182488b08
Joe Julian <me@joejulian.name>
2017-01-26 17:04:02 -0800

    Prevent k2 from stomping on user files (#170)
    * Stop openssh from creating ~/.rnd
    
    openssh creates an entropy file at ${HOME}/.rnd which, in the docker
    container, is owned by root. When this file is in a user-owned
    directory (or home) it can stomp on the user-owned file by the same
    name.
    
    By setting the RANDFILE environment variable, we can force it to use a
    safe filename.
    
    * Make openssh create safe .srl files
    
    openssh x509 was being called with CAcreateserial which was causing it
    to create `~/.srl`. This specifies a proper file location to prevent
    potentially overwriting a user's own .srl file in their home directory.
    
    * Use ${HOME} instead of ~ for consistency and readability
    
    * Stop ansible and terraform from creating files in $HOME
    
    Both ansible and terraform try to create files/directories under `$HOME`
    with no way of reconfiguring that behavior. Change the `$HOME` export to
    allow them a safe place to store their workfiles.

--- bin/utils.sh
+++ bin/utils.sh
@@ -10,6 +10,9 @@ my_dir=$(dirname "${BASH_SOURCE}")
 readonly KRAKEN_ROOT=$(cd "${my_dir}/.."; pwd)
 KRAKEN_VERBOSE=${KRAKEN_VERBOSE:-false}
 
+# set RANDFILE to prevent creation of ${HOME}/.rnd by openssl
+export RANDFILE=$(mktemp)
+
 function warn {
   echo -e "\033[1;33mWARNING: $1\033[0m"
 }
@@ -41,10 +44,10 @@ function show_help {
 
   inf "\nFor example:"
   inf "[up].sh --generate"
-  inf "[up].sh --generate ~/.kraken/myconfig.yaml"
+  inf "[up].sh --generate \${HOME}/.kraken/myconfig.yaml"
   inf ""
-  inf "[up].sh --output ~/.kraken/myclusterstate --config ~/.kraken/myclusterconfig.yaml --tags config,services"
-  inf "[up].sh --config ~/.kraken/myclusterconfig.yaml"
+  inf "[up].sh --output \${HOME}/.kraken/myclusterstate --config \${HOME}/.kraken/myclusterconfig.yaml --tags config,services"
+  inf "[up].sh --config \${HOME}/.kraken/myclusterconfig.yaml"
 }
 
 function show_post_cluster {
@@ -113,13 +116,13 @@ if [ -n "${KRAKEN_GENERATE_PATH+x}" ]; then
 fi
 
 if [ -z ${KRAKEN_CONFIG+x} ]; then
-  warn "--config not specified. Using ~/.kraken/config.yaml as location"
-  KRAKEN_CONFIG="~/.kraken/config.yaml"
+  warn "--config not specified. Using ${HOME}/.kraken/config.yaml as location"
+  KRAKEN_CONFIG="${HOME}/.kraken/config.yaml"
 fi
 
 if [ -z ${KRAKEN_BASE+x} ]; then
-  warn "--output not specified. Using ~/.kraken as location"
-  KRAKEN_BASE="~/.kraken"
+  warn "--output not specified. Using ${HOME}/.kraken as location"
+  KRAKEN_BASE="${HOME}/.kraken"
 fi
 
 if [ -z ${KRAKEN_TAGS+x} ]; then
@@ -127,3 +130,6 @@ if [ -z ${KRAKEN_TAGS+x} ]; then
 fi
 
 KRAKEN_EXTRA_VARS="config_path=${KRAKEN_CONFIG} config_base=${KRAKEN_BASE} "
+
+# We don't want anything else to think home is someplace we should write to. Change it.
+export HOME=$(mktemp -d)
