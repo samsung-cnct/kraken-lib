d5eaffcace21a2f218776985d67ddb2dcc3d94c4
Joe Julian <me@joejulian.name>
2017-03-13 17:04:53 -0700

    Add is_empty test (#244)
    This test eliminates long cumbersome definition tests like
    
    ```
    (kraken_config.providerConfig.type is undefined) or (kraken_config.providerConfig.type is none) or (kraken_config.providerConfig.type|trim == '')
    ```
    with
    
    ```
    kraken_config.providerConfig.type | is_empty
    ```

---
+++ ansible/roles/kraken.config/filter_plugins/is_empty.py
@@ -0,0 +1,24 @@
+# from plugins/filter/json_query.py
+
+from ansible.errors import AnsibleError
+from ansible.plugins.lookup import LookupBase
+from ansible.parsing.yaml.objects import AnsibleUnicode
+from ansible.utils.listify import listify_lookup_plugin_terms
+from jinja2.runtime import StrictUndefined
+
+def is_empty(data):
+    if isinstance(data, dict):
+        return data == {}
+    if isinstance(data, AnsibleUnicode):
+        return data.strip() == ""
+    if isinstance(data, StrictUndefined):
+        return True
+    return False
+
+class FilterModule(object):
+    ''' Query filter '''
+    def filters(self):
+        return {
+            'is_empty': is_empty
+        }
+
