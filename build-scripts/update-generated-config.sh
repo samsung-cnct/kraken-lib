97ae6da806424fc81891a28a895a7dbb7522ee13
davidewatson <davidewatson@gmail.com>
2017-06-13 11:07:23 -0700

    Change default region to us-east-1 and update schema with (#510)
    * Change default region to us-east-1 and update schema with
    documentation and defaults. Fixes #465.
    
    * Force CI jobs to us-east-2.
    
    * Remove dead code for unsupported config property.
    
    * Remove trailing double quote.

--- build-scripts/update-generated-config.sh
+++ build-scripts/update-generated-config.sh
@@ -7,8 +7,9 @@ set -x
 
 cluster_name=`echo $2 | tr -cd '[[:alnum:]]-' | tr '[:upper:]' '[:lower:]'`
 
-#  old style configs (can be removed after k2recon is merged)
-sed -i -e "s/cluster:/cluster: ${cluster_name}/" $1
-
 #  new style config
 sed -i -e "s/- name:[[:space:]]*$/- name: ${cluster_name}/" $1
+
+# move regions and AZs to us-east-2. note that this is the CNCT CI region for
+# API rate limit purposes.
+sed -i -e "s/us-east-1/us-east-2/g" $1
