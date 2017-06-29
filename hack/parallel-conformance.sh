edd871b7152047a7a4ebb53c4dc109be5dea7a63
choused <coffeepac@gmail.com>
2017-06-29 11:45:06 -0700

    rev CoreOS version and sort out e2e.go params (#587)
    * rev CoreOS version
    
    * remove possible offending flag
    
    * use correct variable for not checking version skew
    
    * command line correctness

--- hack/parallel-conformance.sh
+++ hack/parallel-conformance.sh
@@ -46,7 +46,7 @@ function run_hack_e2e_go() {
   
   # run everything that we can in 
   cd ${KUBE_ROOT}
-  GINKGO_PARALLEL=y go run hack/e2e.go --v --test --test_args="${common_test_args[*]} ${test_args[*]}" --check_version_skew=false
+  GINKGO_PARALLEL=y go run hack/e2e.go -- --v --test --test_args="${common_test_args[*]} ${test_args[*]}" --check-version-skew=false
 }
 
 echo
